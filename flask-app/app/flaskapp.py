0426572b9d9799c0bc7e8140c778fcab7f194484
Maximilien Vicari <maximilien.vicari@gmail.com>
2022-01-01 19:12:37 +0100

    first commit: repo structure

---
+++ flask-app/app/flaskapp.py
@@ -0,0 +1,25 @@
+# import function and modules
+from bmicalc import bmi_calculator
+from flask import Flask, jsonify, request
+
+# instantiate a flask object
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def get_inputs():
+    '''
+    Function gets request using flask and return the result of the funtion bmi_calculator()
+    '''
+
+    packet = request.get_json(force=True)
+    weight = packet['weight']
+    height = packet['height']
+
+    bmi = bmi_calculator(weight, height)
+
+    return jsonify(packet, bmi)
+
+# driver function
+if __name__ == '__main__':
+    app.run()
